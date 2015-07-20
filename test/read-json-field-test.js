2eb08ae0dccf8d18695691cacd4e91295cc09537
sanemat <o.gata.ken@gmail.com>
2015-07-21 01:56:52 +0900

    test(readjson): use common-errors

--- test/read-json-field-test.js
+++ test/read-json-field-test.js
@@ -3,35 +3,41 @@ var assert = require('power-assert');
 var shouldRejected = require('promise-test-helper').shouldRejected;
 var readJsonField = require('../lib/read-json-field');
 var objectAssign = require('object-assign');
-var AppError = require('../lib/app-error');
+var errors = require('common-errors');
 
 var validParams = {
   input: 'example',
-  cwd: process.cwd,
+  cwd: process.cwd(),
   field: 'description',
   path: 'test/fixtures/target.json'
 };
 
 it('should be rejected, cwd is required', function () {
-  var params = objectAssign(validParams, { cwd: null });
+  var params = objectAssign({}, validParams, { cwd: null });
   return shouldRejected(readJsonField(params)).catch(function (error) {
-    assert(error instanceof AppError);
+    assert(error instanceof errors.ArgumentNullError);
   });
 });
 it('should be rejected, field is required', function () {
-  var params = objectAssign(validParams, { field: null });
+  var params = objectAssign({}, validParams, { field: null });
   return shouldRejected(readJsonField(params)).catch(function (error) {
-    assert(error instanceof AppError);
+    assert(error instanceof errors.ArgumentNullError);
   });
 });
 it('should be rejected, path is required', function () {
-  var params = objectAssign(validParams, { path: null });
+  var params = objectAssign({}, validParams, { path: null });
   return shouldRejected(readJsonField(params)).catch(function (error) {
-    assert(error instanceof AppError);
+    assert(error instanceof errors.ArgumentNullError);
   });
 });
 it('should be rejected, opt is required', function () {
   return shouldRejected(readJsonField()).catch(function (error) {
-    assert(error instanceof AppError);
+    assert(error instanceof errors.ArgumentNullError);
+  });
+});
+it('should be rejected, module not found', function () {
+  var params = objectAssign({}, validParams, { path: './test/fixtures/not-found.json' });
+  return shouldRejected(readJsonField(params)).catch(function (error) {
+    assert(error instanceof errors.io.FileNotFoundError);
   });
 });
