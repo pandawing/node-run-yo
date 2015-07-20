c9a55cec4e31d506790842295647043be97078d3
sanemat <o.gata.ken@gmail.com>
2015-07-21 04:53:05 +0900

    refactor(readjson): make smaller module

--- cli.js
+++ cli.js
@@ -1,24 +1,24 @@
-//#!/usr/bin/env node
-///*eslint no-console:0*/
-//'use strict';
-//var meow = require('meow');
-//var yo = require('./lib/yo');
-//
-//var cli = meow({
-//  help: [
-//    'Usage',
-//    '  $ yo-yo [input]',
-//    '',
-//    'Examples',
-//    '  $ yo-yo',
-//    '  unicorns & rainbows',
-//    '',
-//    '  $ yo-yo ponies',
-//    '  ponies & rainbows',
-//    '',
-//    'Options',
-//    '  --foo  Lorem ipsum. Default: false'
-//  ]
-//});
-//
-//console.log(yo({}));
+#!/usr/bin/env node
+/*eslint no-console:0*/
+'use strict';
+var meow = require('meow');
+var yoYo = require('./index');
+
+var cli = meow({
+  help: [
+    'Usage',
+    '  $ yo-yo [input]',
+    '',
+    'Examples',
+    '  $ yo-yo',
+    '  unicorns & rainbows',
+    '',
+    '  $ yo-yo ponies',
+    '  ponies & rainbows',
+    '',
+    'Options',
+    '  --foo  Lorem ipsum. Default: false'
+  ]
+});
+
+console.log(yoYo(cli.input[0] || 'unicorns'));
