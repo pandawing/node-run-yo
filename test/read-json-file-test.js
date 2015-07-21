ae520d9b7dd29828d6f1cccb1c6ab1f1bef819c4
sanemat <o.gata.ken@gmail.com>
2015-07-21 16:56:14 +0900

    refactor(test): use describe, easy to read a test result

--- test/read-json-file-test.js
+++ test/read-json-file-test.js
@@ -6,34 +6,36 @@ var readJsonFile = require('../lib/read-json-file');
 var errors = require('common-errors');
 var path = require('path');
 
-it('should be rejected, path is required', function () {
-  var params = { path: null };
-  return shouldRejected(readJsonFile(params)).catch(function (error) {
-    assert(error instanceof errors.ArgumentNullError);
+describe('read json file', function () {
+  it('should be rejected, path is required', function () {
+    var params = { path: null };
+    return shouldRejected(readJsonFile(params)).catch(function (error) {
+      assert(error instanceof errors.ArgumentNullError);
+    });
   });
-});
-it('should be rejected, opt is required', function () {
-  return shouldRejected(readJsonFile()).catch(function (error) {
-    assert(error instanceof errors.ArgumentNullError);
+  it('should be rejected, opt is required', function () {
+    return shouldRejected(readJsonFile()).catch(function (error) {
+      assert(error instanceof errors.ArgumentNullError);
+    });
   });
-});
-it('should be rejected, file load error', function () {
-  var params = { path: './test/fixtures/not-found.json' };
-  return shouldRejected(readJsonFile(params)).catch(function (error) {
-    assert(error instanceof errors.io.FileLoadError);
+  it('should be rejected, file load error', function () {
+    var params = { path: './test/fixtures/not-found.json' };
+    return shouldRejected(readJsonFile(params)).catch(function (error) {
+      assert(error instanceof errors.io.FileLoadError);
+    });
   });
-});
-it('should be fulfilled, relative path', function () {
-  var params = { path: './test/fixtures/target.json' };
-  var expectedData = require(path.join(process.cwd(), './test/fixtures/target.json'));
-  return shouldFulfilled(readJsonFile(params)).then(function (value) {
-    assert.deepEqual(value, expectedData);
+  it('should be fulfilled, relative path', function () {
+    var params = { path: './test/fixtures/target.json' };
+    var expectedData = require(path.join(process.cwd(), './test/fixtures/target.json'));
+    return shouldFulfilled(readJsonFile(params)).then(function (value) {
+      assert.deepEqual(value, expectedData);
+    });
   });
-});
-it('should be fulfilled, absolute path', function () {
-  var params = { path: path.join(process.cwd(), './test/fixtures/target.json') };
-  var expectedData = require(path.join(process.cwd(), './test/fixtures/target.json'));
-  return shouldFulfilled(readJsonFile(params)).then(function (value) {
-    assert.deepEqual(value, expectedData);
+  it('should be fulfilled, absolute path', function () {
+    var params = { path: path.join(process.cwd(), './test/fixtures/target.json') };
+    var expectedData = require(path.join(process.cwd(), './test/fixtures/target.json'));
+    return shouldFulfilled(readJsonFile(params)).then(function (value) {
+      assert.deepEqual(value, expectedData);
+    });
   });
 });
