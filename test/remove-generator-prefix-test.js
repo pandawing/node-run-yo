72eb50a793fb19a22e628f660ab1ec4e1b9fa135
sanemat <o.gata.ken@gmail.com>
2015-07-22 23:59:25 +0900

    refactor(generator): change to more readable name

--- test/remove-generator-prefix-test.js
+++ test/remove-generator-prefix-test.js
@@ -1,22 +1,22 @@
 'use strict';
 var assert = require('power-assert');
-var removePrefixedGenerator = require('../lib/remove-prefixed-generator');
+var removeGeneratorPrefix = require('../lib/remove-generator-prefix');
 var errors = require('common-errors');
 
-describe('remove prefixed generator', function () {
-  it('should be removed generator', function () {
-    removePrefixedGenerator('generator-generator-generator', function (err, data) {
+describe('remove generator prefix', function () {
+  it('should be removed generator prefix', function () {
+    removeGeneratorPrefix('generator-generator-generator', function (err, data) {
       if (err) { throw err; }
       assert.equal(data, 'generator-generator');
     });
   });
   it('should not be valid, empty name', function () {
-    removePrefixedGenerator('generator-', function (err) {
+    removeGeneratorPrefix('generator-', function (err) {
       assert(err instanceof errors.ArgumentError);
     });
   });
   it('should not be valid, prefix exists', function () {
-    removePrefixedGenerator('yo-generator-foo', function (err) {
+    removeGeneratorPrefix('yo-generator-foo', function (err) {
       assert(err instanceof errors.ArgumentError);
     });
   });
