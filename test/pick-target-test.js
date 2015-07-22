51133366543fd71810d4aa1c813c3173c0e42d4c
sanemat <o.gata.ken@gmail.com>
2015-07-22 17:56:32 +0900

    refactor(pick): add describe

--- test/pick-target-test.js
+++ test/pick-target-test.js
@@ -3,18 +3,20 @@ var assert = require('power-assert');
 var pickTarget = require('../lib/pick-target');
 var errors = require('common-errors');
 
-it('should not be allowed null', function () {
-  pickTarget(null, function (error) {
-    assert(error instanceof errors.TypeError);
+describe('pick target', function () {
+  it('should not be allowed null', function () {
+    pickTarget(null, function (error) {
+      assert(error instanceof errors.TypeError);
+    });
   });
-});
-it('should not be allowed (empty)', function () {
-  pickTarget('', function (error) {
-    assert(error instanceof errors.ArgumentError);
+  it('should not be allowed (empty)', function () {
+    pickTarget('', function (error) {
+      assert(error instanceof errors.ArgumentError);
+    });
   });
-});
-it('should not be allowed "."', function () {
-  pickTarget('.', function (error) {
-    assert(error instanceof errors.ArgumentError);
+  it('should not be allowed "."', function () {
+    pickTarget('.', function (error) {
+      assert(error instanceof errors.ArgumentError);
+    });
   });
 });
